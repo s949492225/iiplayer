1884da894f5b7f83697400fae732b5662f0d40e2
宋林涛 <1126151974@qq.com>
2018-11-05 16:51:40 +0800

    解码速度跟不上不丢帧,否则会显示不正常

--- app/src/main/cpp/media/VideoRender.cpp
+++ app/src/main/cpp/media/VideoRender.cpp
@@ -21,7 +21,6 @@ void VideoRender::play() {
 
 void VideoRender::playThread() {
     LOGD("视频播放线程开始,tid:%i\n", gettid());
-    FILE *fp_yuv = fopen("/sdcard/test.yuv", "wb+");
     while (mStatus != NULL && !mStatus->isExit) {
 
         if (mStatus->isSeek) {
@@ -74,7 +73,7 @@ void VideoRender::playThread() {
                                                      frame->data[2]);
             } else {
                 LOGE("当前视频不是YUV420P格式");
-                AVFrame *pFrameYUV420P = av_frame_alloc();
+                AVFrame *yuvFrame = av_frame_alloc();
                 int num = av_image_get_buffer_size(
                         AV_PIX_FMT_YUV420P,
                         mWidth,
@@ -82,8 +81,8 @@ void VideoRender::playThread() {
                         1);
                 uint8_t *buffer = static_cast<uint8_t *>(av_malloc(num * sizeof(uint8_t)));
                 av_image_fill_arrays(
-                        pFrameYUV420P->data,
-                        pFrameYUV420P->linesize,
+                        yuvFrame->data,
+                        yuvFrame->linesize,
                         buffer,
                         AV_PIX_FMT_YUV420P,
                         mWidth,
@@ -99,8 +98,8 @@ void VideoRender::playThread() {
                         SWS_BICUBIC, NULL, NULL, NULL);
 
                 if (!sws_ctx) {
-                    av_frame_free(&pFrameYUV420P);
-                    av_free(pFrameYUV420P);
+                    av_frame_free(&yuvFrame);
+                    av_free(yuvFrame);
                     av_free(buffer);
                     continue;
                 }
@@ -110,42 +109,32 @@ void VideoRender::playThread() {
                         frame->linesize,
                         0,
                         frame->height,
-                        pFrameYUV420P->data,
-                        pFrameYUV420P->linesize);
+                        yuvFrame->data,
+                        yuvFrame->linesize);
 
 
-//                double diff = getFrameDiffTime(frame);
-//
-//                if (diff < -0.01) {
-//                    int sleep = -(int) (diff * 1000);
-//                    if (fabs(sleep) > 50) {
-//                        sleep = 50;
-//                    }
-//                    av_usleep(static_cast<unsigned int>(sleep * 1000));
-//                    if (mStatus == NULL || mStatus->isExit) {
-//                        av_frame_free(&yuvFrame);
-//                        av_frame_free(&frame);
-//                        av_free(buffer);
-//                        break;
-//                    }
-//                } else if (diff > 0.05) {
-//                    av_frame_free(&yuvFrame);
-//                    av_frame_free(&frame);
-//                    av_free(buffer);
-//                    continue;
-//                }
+                double diff = getFrameDiffTime(frame);
+
+                if (diff < -0.01) {
+                    int sleep = -(int) (diff * 1000);
+                    if (fabs(sleep) > 50) {
+                        sleep = 50;
+                    }
+                    av_usleep(static_cast<unsigned int>(sleep * 1000));
+                    if (mStatus == NULL || mStatus->isExit) {
+                        av_frame_free(&yuvFrame);
+                        av_frame_free(&frame);
+                        av_free(buffer);
+                        break;
+                    }
+                }
 
                 mPlayer->getCallJava()->setFrameData(false, mWidth, mHeight,
-                                                     pFrameYUV420P->data[0],
-                                                     pFrameYUV420P->data[1],
-                                                     pFrameYUV420P->data[2]);
-//                int y_size = mWidth * mHeight;
-//                fwrite(yuvFrame->data[0], 1, (size_t) y_size, fp_yuv);
-//                fwrite(yuvFrame->data[1], 1, y_size / 4I, fp_yuv);
-//                fwrite(yuvFrame->data[2], 1, y_size / 4I, fp_yuv);
-
+                                                     yuvFrame->data[0],
+                                                     yuvFrame->data[1],
+                                                     yuvFrame->data[2]);
                 sws_freeContext(sws_ctx);
-                av_frame_free(&pFrameYUV420P);
+                av_frame_free(&yuvFrame);
                 av_free(buffer);
 
             }
